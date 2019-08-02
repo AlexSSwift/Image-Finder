5e3624dee601c015950cf83094a3db8ca33efe94
Alexander Smith <AlexSSwift@gmail.com>
2019-08-02 11:41:03 -0700

    Initial Commit

---
+++ Image Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Finder
+//
+//  Created by Austin Zheng on 8/2/19.
+//  Copyright © 2019 Austin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
